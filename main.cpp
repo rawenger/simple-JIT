3a932f2891a44cd7d2ceb3d228f3153bae171891
Ryan Wenger <ryanwenger@ucsb.edu>
2022-12-17 17:35:04 -0800

    Add `append_code` helper to allow adding multiple bytes at a time to the JIT code generation

--- main.cpp
+++ main.cpp
@@ -5,22 +5,35 @@
 #include <string>
 #include "recurrence.h"
 
+
+// upper bound to compare our optimization to GCC's
+#pragma GCC optimize ("O3")
+// leave the overhead of a function call to make it as accurate as possible
+[[gnu::noinline]]
+double r1(double lastN = 0) {
+        return (((54 + 3) / lastN) - (4 * 2)) + lastN;
+}
+
 int main() {
-        std::string expr = "(((54 + 3) / 8) - (4 * 2)) + n";
-        int nIter = 100000;
+        std::string expr = "(((54 + 3) / n) - (4 * 2)) + n";
+        int nIter = 10000000;
+        double lastN = 1;
         std::cout << "Enter expression: \n";
 //        std::cout << expr << std::endl;
         // test with (((54 + 3) / 8) - (4 * 2)) + n
         // output: [54, 3, +, 8, /, 4, 2, *, -, n, +]
-        std::getline(std::cin, expr);
+//        std::getline(std::cin, expr);
         std::cout << "Number of iterations?\n";
-        std::cin >> nIter;
-        recurrence r(expr);
-//        r.jit_compile();
+//        std::cin >> nIter;
+        recurrence r(expr, lastN);
+        r.jit_compile();
 //        r.print_toks();
 //        r.print_pf();
         std::cout << r.compute(nIter) << std::endl;
-        fflush(nullptr);
-        std::cout << r.compute(nIter, false) << std::endl;
+//        fflush(nullptr);
+//        for (int i = 0; i < nIter; i++)
+//                lastN = r1(lastN);
+//        std::cout << lastN << std::endl;
+//        std::cout << r.compute(nIter, false) << std::endl;
         return 0;
 }
