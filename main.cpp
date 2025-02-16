3b6cbb30dec7f0be514c26e68b0957b565d11496
Ryan Wenger <rawenger@proton.me>
2025-02-16 12:55:29 -0800

    switch main code to parse CLI args

--- main.cpp
+++ main.cpp
@@ -5,22 +5,50 @@
 #include <string>
 #include "recurrence.h"
 
-int main() {
-        std::string expr = "(((54 + 3) / 8) - (4 * 2)) + n";
-        int nIter = 100000;
-        std::cout << "Enter expression: \n";
-//        std::cout << expr << std::endl;
+using namespace std;
+
+[[noreturn]]
+static void usage(const char *progname) {
+        cerr << "usage: " << progname
+                << " [equation [n_iter [N_0]]]"
+                << endl;
+        exit(1);
+}
+
+int main(int argc, char *argv[]) {
         // test with (((54 + 3) / 8) - (4 * 2)) + n
         // output: [54, 3, +, 8, /, 4, 2, *, -, n, +]
-        std::getline(std::cin, expr);
-        std::cout << "Number of iterations?\n";
-        std::cin >> nIter;
+        string expr = "(((54 + 3) / 8) - (4 * 2)) + n";
+        size_t nIter = 100000;
+        double N_0 = 0;
+
+        try {
+                switch (argc) {
+                    case 4:
+                        N_0 = stod(argv[3]);
+                    case 3:
+                        nIter = stoul(argv[2]);
+                    case 2:
+                        expr = argv[1];
+                    case 1:
+                        break;
+                    default:
+                        usage(argv[0]);
+                }
+        } catch (invalid_argument &e) {
+                cerr << e.what() << endl;
+                usage(argv[0]);
+        }
+
+        
         recurrence r(expr);
 //        r.jit_compile();
 //        r.print_toks();
 //        r.print_pf();
-        std::cout << r.compute(nIter) << std::endl;
+        cout << "with JIT: "
+                << r.compute(nIter) << endl;
         fflush(nullptr);
-        std::cout << r.compute(nIter, false) << std::endl;
+        // cout << "without JIT: "
+        //         << r.compute(nIter, false) << endl;
         return 0;
 }
